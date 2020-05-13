832287dbf527bd4d5c62fb457e6ef927db529ac3
Sylvain Gournay <gournay.sylvain@gmail.com>
2020-05-13 09:28:49 +0200

    Creating project structure with Spring Initializr

---
+++ src/main/java/fr/sgo/bcm/flightaggregator/FlightAggregatorApplication.java
@@ -0,0 +1,13 @@
+package fr.sgo.bcm.flightaggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightAggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightAggregatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/sgo/bcm/flightaggregator/FlightAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.sgo.bcm.flightaggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightAggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
